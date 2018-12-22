c179350a3022b224c8c04a035ff4d87416bece40
Teagan Glenn <that@teagantotally.rocks>
2018-12-22 15:11:15 -0700

    Update authenticate platform to be generic and support coroutine

--- src/main/kotlin/state/data/AuthenticatedPlatform.kt
+++ src/main/kotlin/state/data/AuthenticatedPlatform.kt
@@ -3,6 +3,6 @@ package state.data
 import state.data.models.AuthenticationResult
 import state.data.models.Credentials
 
-interface AuthenticatedPlatform : Platform {
-    fun authenticate(credentials: Credentials): AuthenticationResult
+interface AuthenticatedPlatform<CREDENTIALS : Credentials> : Platform {
+    suspend fun authenticate(credentials: CREDENTIALS): AuthenticationResult
 }
