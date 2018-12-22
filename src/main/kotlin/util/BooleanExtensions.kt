6386b595fe03178c7b71fed3e33e845626b7ee12
Teagan Glenn <that@teagantotally.rocks>
2018-12-22 16:40:35 -0700

    Extensions

---
+++ src/main/kotlin/util/BooleanExtensions.kt
@@ -0,0 +1,13 @@
+package util
+
+inline fun Boolean.ifTrue(block: () -> Unit): Unit = if (this) block() else Unit
+
+inline fun <R> Boolean.ifTrueMaybe(block: () -> R): R? = if (this) block() else null
+
+inline fun Boolean.ifTrueAlso(block: () -> Unit): Boolean = this.ifTrueMaybe { this.also { block() } } ?: this
+
+inline fun Boolean.ifFalse(block: () -> Unit): Unit = if (!this) block() else Unit
+
+inline fun <R> Boolean.ifFalseMaybe(block: () -> R): R? = if (!this) block() else null
+
+inline fun Boolean.ifFalseAlso(block: () -> Unit): Boolean = this.ifFalseMaybe { this.also { block() } } ?: this
