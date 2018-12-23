d45e4960ca762b716eddfab479bca80e3e0c5230
Teagan Glenn <that@teagantotally.rocks>
2018-12-22 18:14:35 -0700

    Platform manager

---
+++ src/main/kotlin/platform/PlatformManager.kt
@@ -0,0 +1,19 @@
+package platform
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class PlatformManager(
+        override val coroutineContext: CoroutineContext = Dispatchers.IO + Job()
+) : CoroutineScope {
+    private val platforms: MutableList<Platform> = mutableListOf()
+
+    fun initialize(vararg platform: Platform) {
+        platform.forEach {
+            launch { it.initialize() }
+        }
+    }
+}
